e8771fa83bef451686dba2ffc1f9bb355125f661
nkudlacik <nkudlacik@mcad.edu>
2026-01-24 16:10:25 -0500

    Part 1 Archived as Individual File
    Made separate files for HTML and JS from part 1 checkpoints preserved on Github.

---
+++ wk-1/exercises/snow-creature/part1.js
@@ -0,0 +1,39 @@
+console.log("snowman.js is loaded");
+
+let grid;
+
+function setup() {
+    createCanvas(1000, 800);
+    background("#ccc");
+    grid = loadImage("images/100px_grid.png");
+}
+function draw() {
+    background(grid);
+    //snowman legs
+    fill("#f1f1f1");
+    stroke(34, 95, 199);
+    strokeWeight(20);
+    //left leg
+    ellipse(350, 650, 200);
+    //right leg
+    ellipse(650, 650, 200);
+    //body
+    ellipse(500, 450, 350, 400);
+    //head
+    ellipse(500, 200, 200);
+    //hat brim
+    stroke(87, 51, 16);
+    strokeWeight(40);
+    line(400, 120, 600, 120);
+    //hat body
+    quad(400, 50, 600, 50, 550, 120, 450, 120);
+    //eyes
+    stroke(0);
+    strokeWeight(50);
+    point(425, 200)
+    point(575, 200)
+    //mouth
+    noFill();
+    strokeWeight(10);
+    arc(500, 200, 80, 80, 0, HALF_PI);
+}
