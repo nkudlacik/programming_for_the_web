781df934fbf26b834dcf27029f585bb607ba22b5
nkudlacik <nkudlacik@mcad.edu>
2026-01-24 14:10:49 -0500

    Snowman Base Form Created
    Legs, body, and head created using ellipse.

--- wk-1/exercises/snow-creature/snowman.js
+++ wk-1/exercises/snow-creature/snowman.js
@@ -9,4 +9,16 @@ function setup() {
 }
 function draw() {
     background(grid);
+    //snowman legs
+    fill("#f1f1f1");
+    stroke(34, 95, 199);
+    strokeWeight(20);
+    //left leg
+    ellipse(350, 650, 200);
+    //right leg
+    ellipse(650, 650, 200);
+    //body
+    ellipse(500, 450, 350, 400);
+    //head
+    ellipse(500, 200, 200);
 }
