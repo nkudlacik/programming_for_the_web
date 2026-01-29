d001d194e0f2246872445b36cd2401f784341762
nkudlacik <nkudlacik@mcad.edu>
2026-01-29 18:57:31 -0500

    Prompt Assembly pt I
    Getting foundation of prompt together, still need window message to appear; not sure if I'm doing this right, but wanted to back it up just in case.

--- wk-2/array-of-questions/questions.js
+++ wk-2/array-of-questions/questions.js
@@ -4,15 +4,27 @@ console.log("Hello, world!");
 
 const questions = [
     {
-        question: "How do you say 'Hello' in Polish?",
-        answer: "Cześć",
+        question: "How do you say 'Hello!' in Polish?",
+        answer: "Cześć!",
     },
     {
        question: "How do you say 'How are you?' in Polish?",
        answer: "Jak się masz?", 
     },
     {
-       question: "How do you say 'Have a nice day' in Polish?",
+       question: "How do you say 'Have a nice day!' in Polish?",
        answer: "Miłego dnia!", 
     },
 ];
+console.log(questions);
+
+// Random question prompt, user to respond with guess
+
+const randomIndex = Math.floor(Math.random() * questions.length);
+
+let triviaQuestion = questions[randomIndex];
+
+let userResponse = prompt(triviaQuestion.question);
+
+
+//console.log("random question", questions[randomIndex]);
