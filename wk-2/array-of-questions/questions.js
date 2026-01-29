43426dbfe67d2735116e79fee9adc38db2513e7f
nkudlacik <nkudlacik@mcad.edu>
2026-01-29 18:48:37 -0500

    Array of Question Objects Created
    Using 'question' and 'answer' as the key. Choosing a Polish vocab/flashcard theme for my game.

--- wk-2/array-of-questions/questions.js
+++ wk-2/array-of-questions/questions.js
@@ -1,1 +1,18 @@
 console.log("Hello, world!");
+
+// Create an array of objects representing questions and answers
+
+const questions = [
+    {
+        question: "How do you say 'Hello' in Polish?",
+        answer: "Cześć",
+    },
+    {
+       question: "How do you say 'How are you?' in Polish?",
+       answer: "Jak się masz?", 
+    },
+    {
+       question: "How do you say 'Have a nice day' in Polish?",
+       answer: "Miłego dnia!", 
+    },
+];
