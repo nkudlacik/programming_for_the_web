072a8570eb1c664e728286b2026dbf1450b97229
nkudlacik <nkudlacik@mcad.edu>
2026-02-04 17:35:43 -0500

    Troubleshooting
    My copy of my tile file is significantly larger for no reason in this second exercise and I can't seem to figure out why... the code remains identical.

---
+++ wk-3/parameters/parameters.js
@@ -0,0 +1,37 @@
+function setup() {
+    createCanvas(200,200);
+    noLoop();
+}
+
+
+// creating function
+function createTile() {
+    translate(0, 0);
+    fill('red');
+    rect(0, 0, 200, 200);
+// diagonal lines
+    stroke('blue');
+    strokeWeight(20);
+    line(0, 0, 200, 200);
+    line(200, 0, 0, 200);
+//perpendicular lines
+    stroke('white');
+    strokeWeight(1);
+    line(0, 100, 200, 100);
+    line(100, 0, 100, 200);
+// flower petals
+    noStroke();
+    fill('yellow');
+    circle(100, 75, 40);
+    circle(75, 100, 40);
+    circle(125, 100, 40);
+    circle(100, 125, 40);
+// flower center
+    fill('green');
+    circle(100, 100, 25);
+}
+
+// calling function
+function draw() {
+    createTile();
+}
