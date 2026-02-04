9a8937dd9d39eb743ec6a6ecf278edd25ff43d74
nkudlacik <nkudlacik@mcad.edu>
2026-02-04 17:18:16 -0500

    Flower Tile Designed

--- wk-3/function-tile/tile.js
+++ wk-3/function-tile/tile.js
@@ -1,26 +1,34 @@
 function setup() {
     createCanvas(200,200);
+    noLoop();
 }
 
 
 // creating function
 function createTile() {
     translate(0, 0);
-    fill('blue');
+    fill('red');
     rect(0, 0, 200, 200);
-    stroke('white');
-    strokeWeight(5);
+// diagonal lines
+    stroke('blue');
+    strokeWeight(20);
     line(0, 0, 200, 200);
     line(200, 0, 0, 200);
+//perpendicular lines
+    stroke('white');
+    strokeWeight(1);
+    line(0, 100, 200, 100);
+    line(100, 0, 100, 200);
+// flower petals
     noStroke();
     fill('yellow');
-    circle(100, 50, 50);
-    circle(50, 100, 50);
-    circle(150, 100, 50);
-    circle(100, 150, 50);
-    noFill();
-    stroke('purple');
-    circle(100, 100, 100);
+    circle(100, 75, 40);
+    circle(75, 100, 40);
+    circle(125, 100, 40);
+    circle(100, 125, 40);
+// flower center
+    fill('green');
+    circle(100, 100, 25);
 }
 
 // calling function
