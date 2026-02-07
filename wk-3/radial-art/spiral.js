e927f70939162cfd3b2f3a5bf0619ecc5b660079
nkudlacik <nkudlacik@mcad.edu>
2026-02-07 09:56:45 -0500

    Alternating Color Function
    Each rotation alternates between a new color- yay!!

--- wk-3/radial-art/spiral.js
+++ wk-3/radial-art/spiral.js
@@ -1,4 +1,10 @@
+// SPECIFICATIONS
 let rotateBy = 5;
+let colors = ['#ccff00', '#e90097'];
+let colorIndex = 0;
+let lastRotation = 0;
+
+//FUNCTIONS
 function setup() {
     createCanvas(600, 600);
     background('#36454f');
@@ -6,12 +12,14 @@ function setup() {
     window.alert("Click anywhere on your screen to freeze the Spirograph! Refresh the page to start a new design!");
 }
 
+//DRAWING THE SPIRAL
+
 function makeArm(rotateBy) {
     let alt = Math.round(rotateBy / 360);
     console.log(alt);
     noFill();
-    stroke('#ccff00');
-    strokeWeight(1);
+    stroke(colors[colorIndex]);
+    strokeWeight(0.5);
     ellipse(10, 200 + alt, 450 - alt);
 }
 
@@ -19,8 +27,19 @@ function draw() {
     translate(300, 300);
     rotate(rotateBy);
     makeArm(rotateBy);
+
+//COLOR VARIATION
+
+    let fullRotationCount = Math.floor(rotateBy / 360);
+    if (fullRotationCount > lastRotation) {
+        colorIndex = (colorIndex + 1) % colors.length;
+        lastRotation = fullRotationCount;
+    }
+
     rotateBy += 5;  
 }
+
+//STOP DRAWING ON CLICK
 
 function mousePressed() {
     noLoop();
