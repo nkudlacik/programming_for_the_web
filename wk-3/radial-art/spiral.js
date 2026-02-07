97eead2e8819d445bb011b35ffb443b97382610b
nkudlacik <nkudlacik@mcad.edu>
2026-02-07 10:14:42 -0500

    Code Cleaned
    Tidied up code with comments and made sure to include 3 parameters to my function

--- wk-3/radial-art/spiral.js
+++ wk-3/radial-art/spiral.js
@@ -1,31 +1,40 @@
-// SPECIFICATIONS
+//PROJECT VARIABLES
 let rotateBy = 5;
 let colors = ['#ccff00', '#fff', '#e90097', '#00e9b7', '#000'];
 let colorIndex = 0;
 let lastRotation = 0;
 
-//FUNCTIONS & SETUP
+//CANVAS SETUP
 function setup() {
     createCanvas(600, 600);
     background('#36454f');
     angleMode(DEGREES);
+
+//USER INSTRUCTIONS POP-UP WINDOW
     window.alert("Click anywhere on your screen to freeze the Spirograph! Refresh the page to start a new design!");
 }
 
-//DRAWING THE SPIRAL
-function makeArm(rotateBy) {
-    let alt = Math.round(rotateBy / 360);
-    console.log(alt);
+//DRAWING FUNCTION & ITS PARAMETERS
+function makeArm(length, width, offset) {
     noFill();
     stroke(colors[colorIndex]);
     strokeWeight(0.5);
-    ellipse(10, 200 + alt, 450 - alt);
+    ellipse(offset, length, width);
 }
 
+//DRAWING THE SPIRAL
 function draw() {
-    translate(300, 300);
+    push();
+    translate(width / 2, height / 2);
     rotate(rotateBy);
-    makeArm(rotateBy);
+     
+//MAKE ARM
+    let alt = Math.round(rotateBy / 360);
+    let length = 200 + alt;
+    let widthEllipse = 450 - alt; 
+    let offset = 10;
+    makeArm(length, widthEllipse, offset);
+    pop();
 
 //COLOR VARIATIONS
     let fullRotationCount = Math.floor(rotateBy / 360);
@@ -33,8 +42,7 @@ function draw() {
         colorIndex = (colorIndex + 1) % colors.length;
         lastRotation = fullRotationCount;
     }
-
-    rotateBy += 5;  
+    rotateBy += 5;
 }
 
 //STOP DRAWING ON CLICK
