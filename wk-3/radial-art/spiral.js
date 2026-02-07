96e4ee573a57d5867c2bc7ee7b991adf44cd14b7
nkudlacik <nkudlacik@mcad.edu>
2026-02-07 09:11:18 -0500

    Radial Art Tutorial Followed
    Followed the tutorial on canvas, going to explore the p5.js library to customize this artwork now!

--- wk-3/radial-art/spiral.js
+++ wk-3/radial-art/spiral.js
@@ -1,9 +1,26 @@
+let rotateBy = 5;
 function setup() {
     createCanvas(600, 600);
     background(0);
     angleMode(DEGREES);
 }
 
+function makeArm(rotateBy) {
+    let alt = Math.round(rotateBy / 360);
+    console.log(alt);
+    noFill();
+    stroke(255);
+    strokeWeight(1);
+    ellipse(150, 150 + alt, 150 - alt);
+}
+
 function draw() {
     translate(300, 300);
+    rotate(rotateBy);
+    makeArm(rotateBy);
+    rotateBy += 5;  
+}
+
+function mousePressed() {
+    noLoop();
 }
