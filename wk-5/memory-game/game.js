47017a1ab33cfec9675e366d95acc48e0f7f9caf
nkudlacik <nkudlacik@mcad.edu>
2026-02-19 13:02:27 -0500

    Assignment 5, Step 5, Code Breaking
    In the last 5 minutes of the tutorial, and things are going astray; code broken and nothing displaying.

--- wk-5/memory-game/game.js
+++ wk-5/memory-game/game.js
@@ -5,8 +5,12 @@ let startingX = 100;
 let startingY = 100;
 let cards = [];
 const gameState = {
-
-}
+    totalPairs: 5,
+    flippedCards: [],
+    numMatched: 0,
+    attempts: 0,
+    waiting: false
+};
 
 //CARD IMAGES
 let cardfaceArray = [];
@@ -25,7 +29,6 @@ function preload() {
 //SETUP
 function setup() {
     createCanvas(800, 600);
-    background('#498a41');
     let selectedFaces = [];
     for (let z = 0; z < 5; z++) {
         const randomIdx = floor(random(cardfaceArray.length));
@@ -39,8 +42,8 @@ function setup() {
 
     for (let j = 0; j < 2; j++) {
         for (let i = 0; i < 5; i++) {
-            const faceImg = selectedFaces.pop();
-            cards.push(new Card(startingX, startingY, faceImg));
+            const cardfaceImg = selectedFaces.pop();
+            cards.push(new Card(startingX, startingY, cardfaceImg));
             startingX += 120;
         }
         startingY += 150;
@@ -49,10 +52,35 @@ function setup() {
 }
 
 //MOUSE PRESSED FUNCTION
-function mousePressed() {
+function mousePressed () {
+    if (gameState.waiting) {
+        return;
+    }
+
     for (let k = 0; k < cards.length; k++) {
-        if(cards[k].didHit(mouseX, mouseY)) {
+        if (gameState.flippedCards.length < 2 && cards[k].didHit(mouseX, mouseY)) {
             console.log('flipped', cards[k]);
+            gameState.flippedCards.push(cards[k]);
+        }
+    }
+
+//CHECKING FOR MATCHES
+    if (gameState.flippedCards.length === 2) {
+        if (gameState.flippedCards[0].cardfaceImg === gameState.flippedCards[1].cardfaceImg) {
+//MARKING MATCHES; NO FLIP
+            gameState.flippedCards[0].isMatch === true;
+            gameState.flippedCards[1].isMatch === true;
+//EMPTYING FLIPPED CARDS ARRAY
+            gameState.flippedCards.length = 0;
+// INCREMENTING SCORE
+            gameState.numMatched++;
+            loop();
+        } else {
+            gameState.waiting = true;
+            const loopTimeout = window.setTimeout(() => {
+                loop();
+                window.clearTimeout(loopTimeout);
+            }, 1000)
         }
     }
 }
@@ -66,19 +94,37 @@ class Card {
         this.height = 100;
         this.face = DOWN;
         this.cardfaceImg = cardfaceImg;
+        this.isMatch = false;
         this.show();
     }
 
+//VICTORY MESSAGE
+function draw () {
+    background('#498a41');
+    if (gameState.numMatched === gameState.totalPairs) {
+        fill('yellow');
+        textSize(66);
+        text('You Win!', 400, 425);
+        noLoop();
+    }
+    for (let n = 0; n < cards.length; n++) {
+        if (!cards[n].isMatch) {
+            cards[n].face = DOWN;
+        }
+        cards[n].show();
+    }
+}
+
 //CARD FUNCTIONS & METHODS
     show() {
-        if (this.face === DOWN) {
-            fill('#ffffff');
-            rect(this.x, this.y, this.width, this.height, 10);
-            image(cardback, this.x, this.y);
-        } else {
+        if (this.face === UP || this.isMatch) {
             fill('#f5cf0f');
             rect(this.x, this.y, this.width, this.height, 10);
             image(this.cardfaceImg, this.x, this.y);
+        } else {
+            fill('#ffffff');
+            rect(this.x, this.y, this.width, this.height, 10);
+            image(cardback, this.x, this.y);
         }
     }
 
