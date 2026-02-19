f4a22d1b67d3348b635981d3540ec374a8bd7565
nkudlacik <nkudlacik@mcad.edu>
2026-02-19 10:44:13 -0500

    Assignment 5, Step #2
    Setting up the flip with UP and DOWN const variables and this.flip(); method.

--- wk-5/memory-game/game.js
+++ wk-5/memory-game/game.js
@@ -1,8 +1,18 @@
+//GLOBAL VARIABLES
+let myCard;
+const DOWN = 'down';
+const UP = 'up';
+
 //CANVAS SETUP
 function setup() {
     createCanvas(1000, 500);
     background('#498a41');
-    let myCard = new Card();
+    myCard = new Card();
+}
+
+//MOUSE PRESSED FUNCTION
+function mousePressed() {
+    console.log(myCard.didHit(mouseX, mouseY));
 }
 
 //CARD CLASS
@@ -12,10 +22,36 @@ class Card {
         this.y = 100;
         this.width = 80;
         this.height = 100;
+        this.face = DOWN;
         this.show();
     }
+
     show() {
-        fill('#ffffff');
-        rect(this.x, this.y, this.width, this.height, 10);
+        if (this.face === DOWN) {
+            fill('#ffffff');
+            rect(this.x, this.y, this.width, this.height, 10);
+        } else {
+            fill('#f5cf0f');
+            rect(this.x, this.y, this.width, this.height, 10);
+        }
+    }
+
+    didHit(mouseX, mouseY) {
+        if (mouseX >= this.x && mouseX <= this.x + this.width &&
+           mouseY >= this.y && mouseY <= this.y + this.height) {
+            this.flip();
+            return true;
+           } else {
+            return false;
+           }
+    }
+
+    flip() {
+        if (this.face === DOWN) {
+            this.face = UP;
+        } else {
+            this.face = DOWN;
+        }
+        this.show();
     }
 }
