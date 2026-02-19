c033ab00558e56558b38e7d466ccd9659bdb3306
nkudlacik <nkudlacik@mcad.edu>
2026-02-19 09:03:49 -0500

    Week 5 Quiz, Question 4
    Randomized array of objects

---
+++ wk-5/quiz/question-4/q-4.js
@@ -0,0 +1,5 @@
+const names = ["Lucky", "Lucy", "Limda", "Lacey", "Carl"];
+
+const randomIdx = Math.round(Math.random() * names.length - 1);
+
+console.log('My dog ' + names[randomIdx] + ' is a really good pup.');
