5c0d0beee3df4f50273822dc11b87851b69132e8
nkudlacik <nkudlacik@mcad.edu>
2026-03-25 17:11:28 -0400

    Exercise 9.2
    Validating Alpaca Fan Club Page

---
+++ wk-9/validate/src/App.jsx
@@ -0,0 +1,92 @@
+ {/* IMPORTS */}
+import './App.css'
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+ {/* APP COMPONENT */}
+function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [userData, setUserData] = useState(null);
+
+  function handleMyForm(data) {
+    console.log('this is the data:', data);
+    setUserData(data);
+  }
+  console.log("errors", errors);
+
+  {/* FORM & USER INPUT FIELDS */}
+  return (
+    <div>
+
+      <h1>Tri-State Area Alpaca Fan Club Registration Form</h1>
+
+      <form onSubmit={handleSubmit(handleMyForm)}>
+        <fieldset>
+          <legend>Personal Information</legend>
+
+          <div>
+            <label htmlFor="firstName">First Name (required)</label>
+            <input type="text" id="firstName" {...register("firstName", {required: true})} />
+            {errors.firstName && (<p className="form-error">Please Input Your First Name</p>)}
+          </div>
+
+          <div>
+            <label htmlFor="lastName">Last Name (required)</label>
+            <input type="text" id="lastName" {...register("lastName", {required: true})} />
+            {errors.lastName && (<p className="form-error">Please Input Your Last Name</p>)}
+          </div>
+
+          <div>
+            <label htmlFor="streetAddress">Street Address</label>
+            <input type="text" id="streetAddress" {...register("streetAddress")} />
+          </div>
+
+          <div>
+            <label htmlFor="state">State</label>
+            <select id="state" {...register("state")}>
+              <option value="NY">New York</option>
+              <option value="NJ">New Jersey</option>
+              <option value="CT">Connecticut</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="country">Country</label>
+            <input type="text" id="country" {...register("country")} />
+          </div>
+
+          <div>
+            <p>Favorite Alpaca Colors</p>
+            <label><input type="checkbox" value="white" {...register("favAlpacaColors")} /> White</label>
+            <label><input type="checkbox" value="gray" {...register("favAlpacaColors")} /> Gray</label>
+            <label><input type="checkbox" value="blonde" {...register("favAlpacaColors")} /> Blonde</label>
+            <label><input type="checkbox" value="brunette" {...register("favAlpacaColors")} /> Brunette</label>
+            <label><input type="checkbox" value="black" {...register("favAlpacaColors")} /> Black</label>
+          </div>
+
+        </fieldset>
+
+        <button type="submit">Submit</button>
+      </form>
+
+{ /*DISPLAY USER DATA*/ }
+      {userData && (
+        <div>
+          <h2>Thank You For Your Response!</h2>
+          <p>First Name: {userData.firstName}</p>
+          <p>Last Name: {userData.lastName}</p>
+          <p>Street Address: {userData.streetAddress}</p>
+          <p>State: {userData.state}</p>
+          <p>Country: {userData.country}</p>
+          <p>
+            Favorite Alpaca Colors:{" "}
+            {userData.favAlpacaColors?.join(", ")}
+          </p>
+        </div>
+      )}
+
+    </div>
+  )
+}
+
+export default App;
